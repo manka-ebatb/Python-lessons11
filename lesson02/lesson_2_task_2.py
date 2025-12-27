655a56bf56c6043b3400db3455dc39da874f6ff9
manka-ebatb <kon-v-palto-v-paltishke@yandex.ru>
2025-12-27 20:54:20 +0500

    выполнение домашнего задания

---
+++ lesson02/lesson_2_task_2.py
@@ -0,0 +1,5 @@
+def leap(a):
+    return True if a % 4 == 0 else False
+def is_year_leap():
+    a = int(input("Введите год "))
+    print("Год", a, ":", leap(a))
---
+++ lesson02/lesson_2_task_7.py
@@ -0,0 +1,2 @@
+for c in range(18, 1, -4):
+    print(c)
