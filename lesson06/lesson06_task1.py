d1cb3917c3001f48b4dc1c54994223da612e63b6
manka-ebatb <kon-v-palto-v-paltishke@yandex.ru>
2026-02-19 15:56:17 +0500

    Update lesson06_task1.py

--- lesson06/lesson06_task1.py
+++ lesson06/lesson06_task1.py
@@ -7,8 +7,11 @@ driver = webdriver.Chrome(service=ChromeService
                           (ChromeDriverManager().install()))
 
 driver.get("http://uitestingplayground.com/ajax")
+waiter = WebDriverWait(driver, 16)
 button = driver.find_element(By.CSS_SELECTOR, "#ajaxButton")
 button.click()
-driver.implicitly_wait(16)
+waiter.until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "p.bg-success"))
+)
 print(driver.find_element(By.CSS_SELECTOR, "p.bg-success").text)
 driver.quit()
