3f41d7515bf829d4cf2d5bc0f4094ec3828663fb
manka-ebatb <kon-v-palto-v-paltishke@yandex.ru>
2026-02-19 16:05:55 +0500

    Update lesson06_task2.py

--- lesson06/lesson06_task2.py
+++ lesson06/lesson06_task2.py
@@ -2,13 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService
                           (ChromeDriverManager().install()))
 
 driver.get("http://uitestingplayground.com/textinput")
+waiter = WebDriverWait(driver, 7)
 driver.find_element(By.CSS_SELECTOR, "#newButtonName").send_keys("SkyPro")
 driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
-driver.implicitly_wait(4)
+waiter.until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#updatingButton"),"SkyPro")
+    )
 print(driver.find_element(By.CSS_SELECTOR, "#updatingButton").text)
 driver.quit()
