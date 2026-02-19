89da93dcb3d6b14277c1be2395fdcd9dabfa329a
manka-ebatb <kon-v-palto-v-paltishke@yandex.ru>
2026-02-19 16:04:54 +0500

    Update lesson06_task1.py

--- lesson06/lesson06_task1.py
+++ lesson06/lesson06_task1.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService
                           (ChromeDriverManager().install()))
