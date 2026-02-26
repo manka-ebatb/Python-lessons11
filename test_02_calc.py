a02748e5a9a62b8ac4442bf4829652ca3d2b7b36
manka-ebatb <kon-v-palto-v-paltishke@yandex.ru>
2026-02-26 14:29:27 +0500

    Update test_02_calc.py

--- test_02_calc.py
+++ test_02_calc.py
@@ -7,27 +7,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(service=ChromeService
-                          (ChromeDriverManager().install()))
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-waiter = WebDriverWait(driver, 90)
-driver.find_element(By.CSS_SELECTOR, "#delay").clear()
-driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
-Keys.ENTER
-key = driver.find_elements(By.CSS_SELECTOR, "span.btn-outline-primary")
-operator = driver.find_elements(By.CSS_SELECTOR, "span.operator")
-key[0].click()
-operator[0].click()
-key[1].click()
-driver.find_element(By.CSS_SELECTOR, "span.btn-outline-warning").click()
-waiter.until(
-    EC.invisibility_of_element_located((By.CSS_SELECTOR, "span#spinner"))
-)
-
-
 def testing():
+    driver = webdriver.Chrome(service=ChromeService
+                          (ChromeDriverManager().install()))
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    waiter = WebDriverWait(driver, 90)
+    driver.find_element(By.CSS_SELECTOR, "#delay").clear()
+    driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
+    Keys.ENTER
+    key = driver.find_elements(By.CSS_SELECTOR, "span.btn-outline-primary")
+    operator = driver.find_elements(By.CSS_SELECTOR, "span.operator")
+    key[0].click()
+    operator[0].click()
+    key[1].click()
+    driver.find_element(By.CSS_SELECTOR, "span.btn-outline-warning").click()
+    waiter.until(
+        EC.invisibility_of_element_located((By.CSS_SELECTOR, "span#spinner"))
+    )
+
     res = driver.find_element(By.CSS_SELECTOR, "div.screen").text
     assert res == "15"
 
 
-driver.quit()
+    driver.quit()
+
